b97c6d17ef7b872d0e07cb6c0342b36025786c4d
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-11 14:01:41 +0300

    Reavt Native Snake Game

---
+++ src/components/Game.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import {SafeAreaView,StyleSheet} from "react-native";
+import {Colors} from "../style/colors";
+
+export default function Game():JSX.Element{
+    return (
+        <SafeAreaView style={styles.container}>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:Colors.primary
+    }
+})
