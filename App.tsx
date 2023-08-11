a6035e0e7f41433c80591ee17223503551cac368
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-11 15:54:04 +0300

    React Native Snake Game

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import "react-native-gesture-handler"
+import "react-native-gesture-handler";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Game from "./src/components/Game";
-import {GestureHandlerRootView} from "react-native-gesture-handler";
 
-const App = ()=>(
-    <GestureHandlerRootView style={{flex:1}}>
-      <Game/>
+const App = () => (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+        <Game />
     </GestureHandlerRootView>
-)
+);
 
-export default App
+export default App;
